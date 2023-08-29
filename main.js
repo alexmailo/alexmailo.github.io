aec44705180915fce4abd6b5ddbda46cb45e28b2
alexmailo <alexkpo2020@gmail.com>
2023-08-29 12:54:58 -0400

    changed to correct syllabus

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 // DOM element where the Timeline will be attached
 var container = document.getElementById('visualization');
 
+// TODO: REMOVE THIS KEY
 let AUTH_KEY = "7Q1cUm8svTijxdO7o2IpXM1BwabawohEV8mHz9uG";
 function get() {
 	const url = new URL(
-		"https://courses.ianapplebaum.com/api/syllabus/1"
+		"https://courses.ianapplebaum.com/api/syllabus/4"
 	);
 
 	const headers = {
@@ -28,18 +29,8 @@ function get() {
 			var timeline = new vis.Timeline(container, items, options);
 
 			})
-	// 		var items = new vis.DataSet([
-	// 	{id: 1, content: 'item 1', start: '2014-04-20'},
-	// 	{id: 2, content: 'item 2', start: '2014-04-14'},
-	// 	{id: 3, content: 'item 3', start: '2014-04-18'},
-	// 	{id: 4, content: 'item 4', start: '2014-04-16', end: '2014-04-19'},
-	// 	{id: 5, content: 'item 5', start: '2014-04-25'},
-	// 	{id: 6, content: 'item 6', start: '2014-04-27', type: 'point'}
-	// ]);
-	//
-
-
 
 	}
+
 
 get()
